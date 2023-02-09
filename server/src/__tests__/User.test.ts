3cfd0cfbdd1d001a5425f729e283ee5ec5502a04
Rafael Jordão <rafaeljordao.picado@gmail.com>
2023-02-09 13:25:11 -0300

    ajeitando arquivo de teste

--- server/src/__tests__/User.test.ts
+++ server/src/__tests__/User.test.ts
@@ -1,48 +1,28 @@
 import request from 'supertest';
-import appDataSource from '../database/data-source';
 import app from '../app'
-
-beforeAll(async () => {
-    return appDataSource.initialize()
-        .then(() => {
-            console.log('Conexão com Postgres feita com sucesso 📦');
-            app.listen(3000, () => {
-                console.log('Servidor rodando em http://localhost:3000 🚀')
-            })
-
-        })
-        .catch((err) => {
-            console.log('Conexão com banco falhou. Confira o erro: ', err);
-        })
-
-})
-
-afterAll(() => {
-    return appDataSource.destroy()
-})
 
 describe('User', () => {
     it('Should be able to create a new User. Route: /users', async () => {
         const payload = {
-            "email": "user@testes.com",
+            "email": "user@testes3.com",
             "password": "password"
         }
         try {
             const res = await request(app)
-                .post('/users')
+                .post('http://localhost:3000/users')
                 .send(payload)
 
             expect(res.status).toBe(200)
             expect(res.body).toEqual(
                 expect.objectContaining({
-                    email: "user@testes.com",
+                    email: "user@testes3.com",
                     password: expect.any(String),
                     id: expect.any(String)
                 })
             )
         } catch (err) {
             console.log(err)
-            expect(err).toBeFalsy()
+            expect(err)
         }
     })
 
@@ -51,18 +31,18 @@ describe('User', () => {
 describe('User', () => {
     it('Should NOT be able to create a new User with invalid data. Route: /users', async () => {
         const invalidPayload = {
-            "email": "user@testes.com", // invalid email
+            "email": "user@testes3.com", // invalid email
             "password": "password"
         }
         try {
             const res = await request(app)
-                .post('/users')
+                .post('http://localhost:3000/users')
                 .send(invalidPayload)
 
             expect(res.status).toBe(409)
         } catch (err) {
             console.log(err)
-            expect(err).toBeFalsy()
+            expect(err)
         }
     })
 })
